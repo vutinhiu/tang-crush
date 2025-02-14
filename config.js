d8836ac1e69ccbd03091fef72b2b2f8c9bdf2c0a
hiuhiu <67354122+vutinhiu@users.noreply.github.com>
2025-02-14 08:06:37 +0700

    Update button

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ const CONFIG = {
     title: 'Người đẹp à em phải trả lời thật lòng cho anh biết nha 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Yêu anh quá điii 🙄',
-    btnNo: 'Không, hehe sao ấn được bé ơiii 😒',
+    btnNo: 'Không yêu 😒',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại lừa được anh 🥹',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 8h anh qua đón nhé công chúa.',
+    messDesc: 'Chiều nay 14/2/2025 15h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun 😌',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/e2ee/t/7838240329548182' //link mess. VD: https://m.me/nam.nodemy
 }
