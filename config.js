e51d4f0b5e50bfe6451d59903e40c21e3444831e
hiuhiu <67354122+vutinhiu@users.noreply.github.com>
2026-03-19 21:33:21 +0700

    update

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Bé Khủng Long à!',
-    introDesc: `Chào người đẹp Thái Bình vừa gặp 1 phát thình lình iu ngayyyy`,
-    btnIntro: 'Ấn đây nào bé Trang',
+    introTitle: 'Chào bé Hương!',
+    introDesc: `Ôi người đẹp Thái Bình vừa gặp 1 phát thình lình iu ngayyyy`,
+    btnIntro: 'Ấn đây nào em',
     title: 'Người đẹp à em phải trả lời thật lòng cho anh biết nha 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Yêu anh quá điii 🙄',
+    btnYes: 'Yêu anh mất dồiii 🙄',
     btnNo: 'Không yêu 😒',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại lừa được anh 🥹',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Chiều nay 14/2/2025 15h anh qua đón nhé công chúa.',
+    reply: 'Thật là bất ngờ làm sao, mình đồng ý cho nhau danh phận nhiaaa',
+    mess: 'Anh biết mà 🥰. Mình có danh phận với nhau rồii 😘😘',
+    messDesc: '18h30 tối ngày 22/2/2025 anh qua đón em nhé công chúa.',
     btnAccept: 'Okiiiii lun 😌',
     messLink: 'https://www.facebook.com/messages/e2ee/t/7838240329548182' //link mess. VD: https://m.me/nam.nodemy
 }
